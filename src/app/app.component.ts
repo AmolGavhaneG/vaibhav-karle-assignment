2a4a80e649ff18b5da03358a0838e877064cd8ff
AmolGavhaneG <amol.gavhane434@gmail.com>
2023-08-18 08:31:13 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vaibhav-angular-assignment';
+  menduDataTree: any = []
+  loading: boolean = true;
+  treeHtml: any = '';
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getMenuData().subscribe((data: any) => {
+      console.log(data);
+      this.menduDataTree = data.data;
+      this.treeHtml = this.buildTreeHtml(this.menduDataTree);
+      console.log(this.treeHtml);
+      this.loading = false;
+    })
+  }
+
+  buildTreeHtml(data, parentId = null) {
+    const tree = [];
+
+    for (const item of data) {
+      item.label = item.name;
+      if (item.refMenuId === parentId) {
+        const children = this.buildTreeHtml(data, item.id);
+        if (children.length > 0) {
+          item.children = children;
+        }
+        tree.push(item);
+      }
+    }
+
+    return tree;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NodeTreeComponent } from './node-tree/node-tree.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TreeModule } from 'primeng/tree';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NodeTreeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TreeModule,
+    HttpClientModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
